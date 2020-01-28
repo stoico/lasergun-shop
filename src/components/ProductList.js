4382f933d3d53bf5c1863c9ad76f52a55d8ea7d2
Carmine Di Martino <cardimart@gmail.com>
2020-01-28 17:38:07 +0100

    Refactor ProductList from class to a Hook.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,55 +1,40 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchDataAsync } from '../actions/fetchProducts'
 import './ProductList.css'
 
-class ProductList extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      currency: {
-        euro: '€',
-        swissFranc: 'CHF'
-      }
-    }
+const ProductList = () => {
+  const dispatch = useDispatch()
+  const products = useSelector((state) => state.products)
+
+  const productURLString = '/product/'
+  const currency = {
+    euro: '€',
+    swissFranc: 'CHF'
   }
 
-  componentDidMount = () => {
-    this.props.fetchDataAsync()
-  }
+  useEffect(() => {
+    dispatch(fetchDataAsync())
+    console.log('called useEffect')
+  }, [dispatch])
 
-  render () {
-    const { products } = this.props
-    const productURLString = '/product/'
-    return (
-      <div className='container'>
-        <h1 className='container-title'>Product List</h1>
-        {products.map((product, index) => {
-          return (
-            <Link to={productURLString + product.id} key={index}>
-              <div className='product-item'>
-                <div className='product-image-thumb'><img src={product.images[0].thumb} alt={product.title} /></div>
-                <div className='product-item-title'>{product.title}</div>
-                <div className='product-item-price'>{this.state.currency.euro} {product.price}</div>
-              </div>
-            </Link>)
-        })}
-      </div>
-    )
-  }
+  return (
+    <div className='container'>
+      <h1 className='container-title'>Product List</h1>
+      {products.map((product, index) => {
+        return (
+          <Link to={productURLString + product.id} key={index}>
+            <div className='product-item'>
+              <div className='product-image-thumb'><img src={product.images[0].thumb} alt={product.title} /></div>
+              <div className='product-item-title'>{product.title}</div>
+              <div className='product-item-price'>{currency.euro} {product.price}</div>
+            </div>
+          </Link>)
+      })}
+    </div>
+  )
 }
 
-const mapState = (state) => {
-  return {
-    products: state.products
-  }
-}
-
-const mapDispatchToProps = { fetchDataAsync }
-
-export default connect(
-  mapState,
-  mapDispatchToProps
-)(ProductList)
+export default ProductList
