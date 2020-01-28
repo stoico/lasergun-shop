a17952c5a1d8ca6aff7e7072fa37a70d5ea7134a
Carmine Di Martino <cardimart@gmail.com>
2020-01-28 07:07:12 +0100

    Create Redux store and provide to the components.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ProductList from './ProductList';
-import * as serviceWorker from './serviceWorker';
+/* eslint-disable no-undef */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import combinedReducer from './reducers/index'
 
-ReactDOM.render(<ProductList />, document.getElementById('root'));
+import './index.css'
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(combinedReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
