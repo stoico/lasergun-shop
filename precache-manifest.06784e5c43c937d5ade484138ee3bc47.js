46a5cb03f055e469ba7e9ff0d12829d5d14657a4
Carmine Di Martino <cardimart@gmail.com>
2020-01-28 07:37:23 +0100

    Updates

---
+++ precache-manifest.06784e5c43c937d5ade484138ee3bc47.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e554c699570f6d553a1ed5a4864dfe4",
+    "url": "/lasergun-shop/index.html"
+  },
+  {
+    "revision": "56c7c21b9d4c997c9f47",
+    "url": "/lasergun-shop/static/css/main.d9fa1a70.chunk.css"
+  },
+  {
+    "revision": "27dd3c7f5b87d80058d5",
+    "url": "/lasergun-shop/static/js/2.93f9d472.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/lasergun-shop/static/js/2.93f9d472.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56c7c21b9d4c997c9f47",
+    "url": "/lasergun-shop/static/js/main.40681eab.chunk.js"
+  },
+  {
+    "revision": "95d9d92768516388c0b9",
+    "url": "/lasergun-shop/static/js/runtime-main.640bd124.js"
+  },
+  {
+    "revision": "34601718b8054893f5a1ea12dc39c235",
+    "url": "/lasergun-shop/static/media/avatar.34601718.svg"
+  },
+  {
+    "revision": "081fb967bb978287daf57b18ec7ff8a8",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-700.081fb967.svg"
+  },
+  {
+    "revision": "704fc425c3cf762e12937a19bce99768",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-700.704fc425.woff2"
+  },
+  {
+    "revision": "80dd776d638f1cce7f7b13dd9ff84095",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-700.80dd776d.eot"
+  },
+  {
+    "revision": "8564aecc3a2046cf3bb9d523991a8b84",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-700.8564aecc.ttf"
+  },
+  {
+    "revision": "a804c575a27cd248e98614b9a81f7a63",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-700.a804c575.woff"
+  },
+  {
+    "revision": "17de587e8b9523678c59f37252064f44",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-900.17de587e.svg"
+  },
+  {
+    "revision": "42fc8f4d91b58a25c7d3800d3e8d42ca",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-900.42fc8f4d.woff2"
+  },
+  {
+    "revision": "4bc63408c4601297b8868a32a04afeb0",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-900.4bc63408.ttf"
+  },
+  {
+    "revision": "c0d751f2968f29d020991e99e6a6fb53",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-900.c0d751f2.woff"
+  },
+  {
+    "revision": "e6885fd5990c6f870b9318380235573a",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-900.e6885fd5.eot"
+  },
+  {
+    "revision": "2de9526f24daf2ba7d96c376f83b781a",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-regular.2de9526f.ttf"
+  },
+  {
+    "revision": "6ef25a3bfae0b14be63f74a238c17fdd",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-regular.6ef25a3b.eot"
+  },
+  {
+    "revision": "811f6a5bb8162b82d1922ce2b8be6888",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-regular.811f6a5b.woff2"
+  },
+  {
+    "revision": "d8bc9340523c9d8c5c0faedca620dcff",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-regular.d8bc9340.woff"
+  },
+  {
+    "revision": "f6af3afb192e87d946ddb020f3114abe",
+    "url": "/lasergun-shop/static/media/titillium-web-v8-latin-regular.f6af3afb.svg"
+  }
+]);
